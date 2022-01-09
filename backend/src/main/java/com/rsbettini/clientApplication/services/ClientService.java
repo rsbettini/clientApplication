5a7845261f8c7ed6ec27bd540feacedd3c39aec4
rsbettini <robson.bettini@hotmail.com>
2022-01-09 19:53:45 -0300

    Implements DTO

--- backend/src/main/java/com/rsbettini/clientApplication/services/ClientService.java
+++ backend/src/main/java/com/rsbettini/clientApplication/services/ClientService.java
@@ -1,23 +1,27 @@
 package com.rsbettini.clientApplication.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.rsbettini.clientApplication.dto.ClientDTO;
 import com.rsbettini.clientApplication.entities.Client;
 import com.rsbettini.clientApplication.repositories.ClientRepository;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
-		return repository.findAll();
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).
+				collect(Collectors.toList());
 	}
 
 }
